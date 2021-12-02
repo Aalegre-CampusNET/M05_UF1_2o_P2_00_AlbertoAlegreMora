b7fe6814a2a45243af2150a6fa4b38731a0dab5b
CampusNET.es <CampusNET.es@DESKTOP-5901C4A>
2021-12-02 19:36:56 +0100

    Codigo Clase

--- PacmanParte1/main.cpp
+++ PacmanParte1/main.cpp
@@ -1,4 +1,7 @@
 #include "Map.h"
+#include "Enemy.h"
+#include "TimeManager.h"
+#include <vector>
 
 /// <summary>
 /// Sets the needed variables
@@ -19,6 +22,7 @@ void Draw();
 
 enum USER_INPUTS { NONE, UP, DOWN, RIGHT, LEFT, QUIT };
 Map pacman_map = Map();
+std::vector<Enemy> enemies;
 char player_char = 'O';
 int player_x = 1;
 int player_y = 1;
@@ -41,8 +45,22 @@ int main()
 void Setup()
 {
     std::cout.sync_with_stdio(false);
+
+    srand(time(NULL));
+
     player_x = pacman_map.spawn_player.X;
     player_y = pacman_map.spawn_player.Y;
+
+    int totalEnemies = 0;
+    std::cout << "Cuantos enemigos quieres? ";
+    std::cin >> totalEnemies;
+    if (totalEnemies < 1)
+        totalEnemies = 1;
+    for (size_t i = 0; i < totalEnemies; i++)
+    {
+        enemies.push_back(Enemy(pacman_map.spawn_enemy, 5.0f * i));
+    }
+
 }
 
 void Input()
@@ -125,6 +143,14 @@ void Logic()
             player_points++;
             pacman_map.SetTile(player_x_new, player_y_new, Map::MAP_TILES::MAP_EMPTY);
             break;
+        case Map::MAP_TILES::MAP_POWERUP:
+            player_points += 100;
+            pacman_map.SetTile(player_x_new, player_y_new, Map::MAP_TILES::MAP_EMPTY);
+            for (size_t i = 0; i < enemies.size(); i++)
+            {
+                enemies[i].PowerUpPicked();
+            }
+            break;
         }
 
         player_y = player_y_new;
@@ -133,6 +159,25 @@ void Logic()
         {
             win = true;
         }
+
+
+        for (size_t i = 0; i < enemies.size(); i++)
+        {
+            switch (enemies[i].Update(&pacman_map, { (short)player_x , (short)player_y }))
+            {
+            case Enemy::ENEMY_STATE::ENEMY_EATED:
+                player_x = pacman_map.spawn_player.X;
+                player_y = pacman_map.spawn_player.Y;
+                break;
+            case Enemy::ENEMY_STATE::ENEMY_KILLED:
+                enemies[i].Kill();
+                player_points += 50;
+                break;
+            default:
+                break;
+            }
+        }
+
     }
 }
 
@@ -143,6 +188,10 @@ void Draw()
     ConsoleUtils::Console_SetPos(player_x, player_y);
     ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::DARK_YELLOW);
     std::cout << player_char;
+    for (size_t i = 0; i < enemies.size(); i++)
+    {
+        enemies[i].Draw();
+    }
     ConsoleUtils::Console_ClearCharacter({ 0,(short)pacman_map.Height });
     ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::CYAN);
     std::cout << "Puntuacion actual: " << player_points << " Puntuacion pendiente: " << pacman_map.points << std::endl;
@@ -151,4 +200,10 @@ void Draw()
         ConsoleUtils::Console_SetColor(ConsoleUtils::CONSOLE_COLOR::GREEN);
         std::cout << "Has ganado!" << std::endl;
     }
+
+    std::cout << TimeManager::getInstance().frameCount << std::endl;
+    std::cout << TimeManager::getInstance().deltaTime << std::endl;
+    std::cout << TimeManager::getInstance().time << std::endl;
+
+    TimeManager::getInstance().NextFrame();
 }
